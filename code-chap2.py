d8986278d9fe5aa0095ad6484ca26d2dff95c306
NStavrakoudis <72755072+NStavrakoudis@users.noreply.github.com>
2021-01-04 10:28:25 +0200

    Delete code-chap2.py

--- code-chap2.py
+++
@@ -1,8 +0,0 @@
-""" This program shows how to get input from the keyboard 
-with Python. It gets a message, it prints it twice. Then, 
-it gets a number, it adds 1 to it, and prints back."""
-
-message = input("Enter a message: ")
-print(message * 2)
-number = int(input("Enter a number: "))
-print(number + 1)
